40d3c991f99a9ad7c9df38b9aa34dc2c498510e8
Kevin Orta <orta.kevin@gmail.com>
2021-12-22 08:32:36 -0600

    still learning node, entering express

---
+++ index.js
@@ -0,0 +1,11 @@
+var superheros = require("superheroes");
+
+var mysuperheroname = superheros.random();
+
+console.log(mysuperheroname);
+
+var superVillian = require("supervillains");
+
+var mySuperVillain = superVillian.random();
+
+console.log(mySuperVillain);
